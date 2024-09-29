e039648dcb7e9abde47f2d3aee6f35659c6a5be7
felix-scl <fhernan@live.cl>
2024-09-28 22:36:34 -0300

    refactor: update GetOtp route from root to /get-otp

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,12 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
-import Otp from "../pages/Otp";
+import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Profile from "../pages/Profile";
+import GetOtp from "../pages/GetOtp";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <Otp />,
+    element: <Home />,
+  },
+  {
+    path: "/get-otp",
+    element: <GetOtp />,
   },
   {
     path: "/login",
