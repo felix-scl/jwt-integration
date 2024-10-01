94e3c57417a78f2c7fde04ef13ae80e1fe18364e
felix-scl <fhernan@live.cl>
2024-09-30 22:57:01 -0300

    styles: add basic colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,19 @@ import daisyui from "daisyui";
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        lato: ["Lato", "sans-serif"],
+        rammetto: ["Rammetto One", "sans-serif"],
+        josefina: ["Josefin Sans", "sans-serif"],
+      },
+      colors: {
+        "black-primary": "#0E0F0C",
+        "black-secondary": "#163300",
+        "grey-primary": "#454745",
+        "green-primary": "#9FE870",
+      },
+    },
   },
   daisyui: {
     themes: ["garden"],
