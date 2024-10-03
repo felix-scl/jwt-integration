d0da7cf0d333e839bb646fc729a61f5ef39eb159
felix-scl <fhernan@live.cl>
2024-10-03 06:24:15 -0300

    feat: add intercept functionality to send the Bearer authentication code on every request

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,20 +1,60 @@
 import axios from "axios";
 
 const BASE_URL = import.meta.env.VITE_BACKEND_URL;
+const REFRESH_TOKEN_URL = "/users/token/refresh/";
+const LOGOUT_URL = "/users/logout/";
 
 const api = axios.create({
   baseURL: BASE_URL,
 });
 
-axios.interceptors.request.use(
+api.interceptors.request.use(
   (config) => {
     const accessToken = localStorage.getItem("accessToken");
     if (accessToken) {
       config.headers.Authorization = `Bearer ${accessToken}`;
     }
+
     return config;
   },
   (error) => {
+    return Promise.reject(error);
+  },
+);
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response.status === 401 && !originalRequest._retry) {
+      const refreshToken = localStorage.getItem("refreshToken");
+
+      try {
+        const response = await api.post(REFRESH_TOKEN_URL, {
+          refresh: refreshToken,
+        });
+        const { access, refresh } = response.data;
+
+        localStorage.setItem("accessToken", access);
+        localStorage.setItem("refreshToken", refresh);
+
+        originalRequest.headers.Authorization = `Bearer ${access}`;
+        originalRequest._retry = true;
+
+        return api(originalRequest);
+      } catch {
+        const response = await api.post(LOGOUT_URL, {
+          refresh: refreshToken,
+        });
+
+        if (response.status === 200) {
+          localStorage.removeItem("accessToken");
+          localStorage.removeItem("refreshToken");
+        }
+      }
+    }
+
     return Promise.reject(error);
   },
 );
