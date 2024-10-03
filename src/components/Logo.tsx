39687c918ea1162daa8107d1ff80d539bcbd6430
felix-scl <fhernan@live.cl>
2024-10-02 23:36:32 -0300

    refactor: change anchor tag to NavLink

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,9 +1,11 @@
+import { NavLink } from "react-router-dom";
+
 function Logo() {
   return (
-    <a href="#" className="font-josefin text-xl font-bold">
+    <NavLink to="/" className="font-josefin text-xl font-bold">
       <span className="hidden uppercase tracking-widest sm:inline">Wise</span>
       <span className="sm:hidden">W</span>
-    </a>
+    </NavLink>
   );
 }
 
